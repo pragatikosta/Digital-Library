e6ed15f41524e77e8687ac7e260ec0f9b1b33b7c
pragati <pragatikosta6@gmail.com>
2025-04-18 21:04:01 +0530

    removing src file

--- src/main/java/com/geek/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.geek.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.geek.entity.User;
-
-public interface UserRepository  extends JpaRepository<User,Integer>{
-	 User findByUserName(String uname);
-}
